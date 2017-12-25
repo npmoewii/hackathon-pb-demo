8e5ed4856ab5c13cd63f2ab7570d52e88ce90f48
ongss <ong54321@hotmail.com>
2017-12-26 01:23:03 +0700

    add popbus & station oop

--- logic.js
+++ logic.js
@@ -1,9 +1,17 @@
 window.onload = function(){
-	console.log("hello js");
-	setProgress(1,50);
+	var station = [];
+	var popbus = [];
+	
+
+	// test
+	var pop = new PopBus(1,0,0,true,10,20,"หอใน");
+	console.log(pop.cntNextStation("สยาม"));
+	setProgress(1,100);
 }
 
-const station = ["สยาม","ศาลาพระเกี้ยว","หอใน"];
+// FUNCTION
+
+const stationName = ["สยาม","ศาลาพระเกี้ยว","หอใน"];
 
 function setProgress(id,value) {
 	let pro = document.getElementById(id);
@@ -17,15 +25,59 @@ function setProgress(id,value) {
 	},10)
 }
 
-function popBus(id,lat,long,weight,maxWeight){
-	var id = id;
-	var lat = lat;
-	var long = long;
-	var personCnt = Math.floor(weight/60);
-	var maxPersonCnt = Math.floor(maxWeight/60);
+// CLASS
 
+function PopBus(id,lat,long,status,weight,maxWeight,station){
+	this.id = id;
+	this.lat = lat;
+	this.long = long;
+	this.status;
+	this.station = station;
+	this.personCnt = Math.floor(weight/60);
+	this.maxPersonCnt = Math.floor(maxWeight/60);
+
+	this.findPos = function(name){
+		for(let i=0;i<stationName.length;i++){
+			if(stationName[i] === name) return i;	
+		} 
+	}
+
+	this.cntNextStation = function(name){
+		let cnt = 0;
+		let start = this.findPos(this.station);
+		while(cnt < stationName.length){
+			if(stationName[(start+cnt)%stationName.length] === name) return cnt;
+			cnt++;
+		}
+	}
+
+	this.createHtmlElement = function(){
+		//TODO GENERATE HTML ELEMENT AND ADDED IN PRARENT
+	}
 }
 
-function compare(a,b){
-	if(a)
+function Station(name){
+	this.name = name;	
+	
+	this.compare = function(popBusA,popBusb){
+		let cntA = popBusA.cntNextStation(this.name);
+		let cntB = popBusB.cntNextStation(this.name);
+		if(cntA > cntB) return 1;
+		else if(cntA < cntB) return -1;
+		else if(cntA === cntB){
+			if(popBusA.personCnt > popBusB.personCnt) return 1;
+			else if(popBusA.personCnt < popBusB.personCnt) return -1;	
+			else if(popBusA.personCnt === popBusB.personCnt) return 0;
+		}
+	}
+
+	this.createHtmlElement = function(){
+		//TODO GENERATE HTML ELEMENT AND ADDED IN PRARENT
+	}
+}
+
+function StationSelector(){
+	this.init = fucntion(){
+		//TODO INIT STATION SELECTOR
+	}
 }
