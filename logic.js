1d3eb70b7e924de474c91574d3f391c022f43e45
ongss <ong54321@hotmail.com>
2017-12-25 23:48:07 +0700

    add animate progress bar

--- logic.js
+++ logic.js
@@ -1,5 +1,31 @@
 window.onload = function(){
 	console.log("hello js");
-	const pro1 = document.getElementById("1");
-	pro1.style.width = "50%";
+	setProgress(1,50);
+}
+
+const station = ["สยาม","ศาลาพระเกี้ยว","หอใน"];
+
+function setProgress(id,value) {
+	let pro = document.getElementById(id);
+	let pos = pro.style.width;
+	let val = Math.floor(value)%100; 
+	let animate = setInterval(function(){
+		if(pos > value) pos--;
+		else if(pos < value) pos++;
+		pro.style.width = pos + '%';
+		if(pos === value || pos === 0) clearInterval(animate);
+	},10)
+}
+
+function popBus(id,lat,long,weight,maxWeight){
+	var id = id;
+	var lat = lat;
+	var long = long;
+	var personCnt = Math.floor(weight/60);
+	var maxPersonCnt = Math.floor(maxWeight/60);
+
+}
+
+function compare(a,b){
+	if(a)
 }
